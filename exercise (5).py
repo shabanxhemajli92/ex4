3bd37b56624a8c4da9c8c8074f7b583ca4995bc1
Shaban <shabanxhemajli@live.com>
2022-08-12 11:52:02 +0200

    shhshshsh

--- exercise (5).py
+++ exercise (5).py
@@ -1,4 +1,4 @@
-#Task 2
+'''#Task 
 
 for i in range(3):
 
@@ -18,5 +18,18 @@ for i in range(3):
     if    y != z:
         print("The second numer is not equal to the first")
     if 10 > y > 100:
-        print("This checks if the number is in the range ")
+        print("This checks if the number is in the range ") '''
+                                                                          
+#Task 2
 
+print("List of months: January, February, March, April, May, June, July, August, September, October, November, December")
+month_name = input("Input the name of Month: ")
+ 
+if month_name == "February":
+    print("No. of days: 28/29 days")
+elif month_name in ("April", "June", "September", "November"):
+    print("No. of days: 30 days")
+elif month_name in ("January", "March", "May", "July", "August", "October", "December"):
+    print("No. of days: 31 day")
+else:
+    print("Wrong month name")
