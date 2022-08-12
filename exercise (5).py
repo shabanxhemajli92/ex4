ed2b9b205ed06c6c74f0a547aeafd52cb9ac409c
Shaban <shabanxhemajli@live.com>
2022-08-12 11:24:26 +0200

    fwww

--- exercise (5).py
+++ exercise (5).py
@@ -1,3 +1,22 @@
-# do exercise here
-# upload to github for portfolio
-# thank me later when your git graph is as green as the python money u gonna earn
+#Task 2
+
+for i in range(3):
+
+    z=int(input("Enter the first number: "))
+    y=int(input("Enter the second number: "))
+
+    if z > y:
+            print("The first number is greater than the second")
+    if y > z:
+        print("The second number is greater than the first")
+    if z >= y:
+        print("The first number is either greater or equal")
+    if y >= z:
+        print("The second number greater or equal than the first:")
+    if z != y:
+        print("The first number is not equal to the second")
+    if    y != z:
+        print("The second numer is not equal to the first")
+    if 10 > y > 100:
+        print("This checks if the number is in the range ")
+
